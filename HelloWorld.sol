e6499595c2522395e963c81f8f215239a09c52a4
"Olatunde Success" <"olatundesuccess54@gmail.com">
2022-03-09 00:07:00 +0100

    HelloWorld contract updated

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -3,16 +3,20 @@ pragma solidity >=0.6.0 <0.9.0;
 
 contract HelloWorld{
     
-    string yourName;
+    //a state variable
+    string yourName; 
 
+    //  a constructor
     constructor (){
         yourName = "Hello World!";
-    }
+    } 
 
+     // a Function => to display the stored name
      function displayName() public view returns (string memory){
         return yourName;
-    }
+    } 
 
+    // Another function => to store name
      function storeName(string memory _name) public{
         yourName = _name;
     }
